575389e3e5a4eee1b50fbdd168a5514ca1bec0bd
ceng517520 <ceng517520@gmail.com>
2023-06-14 14:13:01 +0800

    init

---
+++ A892013-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A892013_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
